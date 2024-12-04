b19cae8bd8133f4c40f9c2fcb13a75eaf35ad98a
Anya <zhangyazhen@zhangyahendeMBP.home>
2024-12-04 19:40:32 +0800

    first commit

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,6 +1,8 @@
 let title = document.getElementById('title_img');
 let fg1 = document.getElementById('fg-right');
 let fg2 = document.getElementById('fg-left');
+let fg1md = document.getElementById('fg-right-md');
+let fg2md = document.getElementById('fg-left-md');
 let cloud = document.getElementById('cloud');
 
 window.addEventListener('scroll', () => {
@@ -9,6 +11,8 @@ window.addEventListener('scroll', () => {
     title.style.marginTop = value * 2 + 'px';
     fg1.style.left = value * 1.5 + 'px';
     fg2.style.left = value * -1.5 + 'px';
+    fg1md.style.left = value * 1.5 + 'px';
+    fg2md.style.left = value * -1.5 + 'px';
     cloud.style.top = value * -1.5 + 'px';
 
 });
